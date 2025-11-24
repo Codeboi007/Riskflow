738a16334139c7f15201bf58c3ed6afeffaebb70
Codeboi007 <shreyaskatti958@gmail.com>
2025-11-24 07:57:13 +0000

    stress test done

--- src/model/model.py
+++ src/model/model.py
@@ -1,7 +1,7 @@
+# src/model/gnn_model.py
 import torch
 import torch.nn as nn
 from torch_geometric.nn import GCNConv
-
 
 class RiskGCN(nn.Module):
     def __init__(self, in_channels, hidden_channels=32, dropout=0.2):
@@ -12,16 +12,11 @@ class RiskGCN(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x, edge_index, edge_weight=None):
-        # First GCN layer
         x = self.conv1(x, edge_index, edge_weight)
         x = torch.relu(x)
         x = self.dropout(x)
-
-        # Second GCN layer
         x = self.conv2(x, edge_index, edge_weight)
         x = torch.relu(x)
         x = self.dropout(x)
-
-        # Node-level prediction
-        out = self.lin(x).view(-1)  # shape [num_nodes]
-        return out  # logits (for BCEWithLogitsLoss)
+        out = self.lin(x).view(-1)
+        return out
